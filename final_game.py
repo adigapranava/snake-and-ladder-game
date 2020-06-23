016845b61661c5f14d13800cfae27f9200d8d1a2
adigapranava <66902872+adigapranava@users.noreply.github.com>
2020-06-23 11:41:45 +0530

    updated
    fixed some bugs related to ending point

--- final_game.py
+++ final_game.py
@@ -86,16 +86,18 @@ screen.setup(420,420)
 p1 = turtle.Turtle()
 p1.shape("turtle")
 p1.color("black")
-p1.speed(1)
+p1.speed(0)
 p1.penup()
 p1.setpos(STARTx + X/2, STARTy +Y/2)
+p1.speed(1)
 
 p2 = turtle.Turtle()
 p2.shape("turtle")
 p2.color("violet")
-p2.speed(1)
+p2.speed(0)
 p2.penup()
 p2.setpos(STARTx + X/2, STARTy +Y/2)
+p2.speed(1)
 
 def position1(SCOREP1):
     if SCOREP1//10 % 2 == 0 and SCOREP1%10 != 0:
@@ -134,10 +136,12 @@ def rolling():
 def rolled():
     global TURN, FORWARD1, FORWARD2, SCOREP1, SCOREP2, OLD_CLICKS, NEW_CLICKS, JUST_STARTED1, CHANGED1, JUST_STARTED2, CHANGED2, PERMISSION1, PERMISSION2
     die = rolling()
-    if SCOREP1 + die > 100:
+    PERMISSION1 = True
+    PERMISSION2 = True
+    if SCOREP1 + die > 100 and TURN%2 == 0:
             print("you cant exceed 100 :)")
             PERMISSION1 = False
-    if SCOREP2 + die > 100:
+    if SCOREP2 + die > 100 and TURN%2 == 1:
             print("you cant exceed 100 :)")
             PERMISSION2 = False
     if TURN%2 == 0 and OLD_CLICKS != NEW_CLICKS and PERMISSION1 == True:            
@@ -161,7 +165,6 @@ def rolled():
         SCOREP1 = check_ladder(SCOREP1)
         position1(SCOREP1)
         print("p1",SCOREP1)
-        TURN += 1
         NEW_CLICKS += 1
         if SCOREP1 != 1:
             JUST_STARTED1 = False
@@ -186,11 +189,10 @@ def rolled():
         SCOREP2 = check_ladder(SCOREP2)
         position2(SCOREP2)
         print("p2",SCOREP2)
-        TURN += 1
         NEW_CLICKS += 1
         if SCOREP2 != 1:
             JUST_STARTED2 = False
-
+    TURN += 1
 
 
 turtle.listen()
