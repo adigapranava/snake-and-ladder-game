36b432be22b79b70f74c6c2fcd7065f69176fbd0
adigapranava <66902872+adigapranava@users.noreply.github.com>
2020-06-23 11:21:02 +0530

    Update final_game.py
    now my turtle climbs the ladder and comes down through snake 
    B-)

--- final_game.py
+++ final_game.py
@@ -97,6 +97,34 @@ p2.speed(1)
 p2.penup()
 p2.setpos(STARTx + X/2, STARTy +Y/2)
 
+def position1(SCOREP1):
+    if SCOREP1//10 % 2 == 0 and SCOREP1%10 != 0:
+        p1.goto(STARTx + X/2+ SCOREP1%10 * X, STARTy - Y/2 + (SCOREP1//10 + 1) * Y)
+        p1.setheading(0)
+    elif SCOREP1//10%2 == 1 and SCOREP1%10 != 0:
+        p1.goto(STARTx + 3*X/2 + (10 - SCOREP1%10) * X, STARTy - Y/2 + (SCOREP1//10 + 1) * Y)
+        p1.setheading(180)
+    elif SCOREP1//10 % 2 == 0 and SCOREP1%10 == 0:
+        p1.goto(STARTx + 3*X/2+ SCOREP1%10 * X, STARTy - 3*Y/2 + (SCOREP1//10 + 1) * Y)
+        p1.setheading(180)
+    elif SCOREP1//10%2 == 1 and SCOREP1%10 == 0:
+        p1.goto(STARTx + X/2 + (10 - SCOREP1%10) * X, STARTy - 3*Y/2 + (SCOREP1//10 + 1) * Y)
+        p1.setheading(0)
+
+def position2(SCOREP1):
+    if SCOREP1//10 % 2 == 0 and SCOREP1%10 != 0:
+        p2.goto(STARTx + X/2+ SCOREP1%10 * X, STARTy - Y/2 + (SCOREP1//10 + 1) * Y)
+        p2.setheading(0)
+    elif SCOREP1//10%2 == 1 and SCOREP1%10 != 0:
+        p2.goto(STARTx + 3*X/2 + (10 - SCOREP1%10) * X, STARTy - Y/2 + (SCOREP1//10 + 1) * Y)
+        p2.setheading(180)
+    elif SCOREP1//10 % 2 == 0 and SCOREP1%10 == 0:
+        p2.goto(STARTx + 3*X/2+ SCOREP1%10 * X, STARTy - 3*Y/2 + (SCOREP1//10 + 1) * Y)
+        p2.setheading(180)
+    elif SCOREP1//10%2 == 1 and SCOREP1%10 == 0:
+        p2.goto(STARTx + X/2 + (10 - SCOREP1%10) * X, STARTy - 3*Y/2 + (SCOREP1//10 + 1) * Y)
+        p2.setheading(0)
+
 def rolling():
     n= random.randint(1,10000)
     print(n%5 + 1)
@@ -104,58 +132,64 @@ def rolling():
 
 
 def rolled():
-	global TURN, FORWARD1, FORWARD2, SCOREP1, SCOREP2, OLD_CLICKS, NEW_CLICKS, JUST_STARTED1, CHANGED1, JUST_STARTED2, CHANGED2, PERMISSION1, PERMISSION2
-	die = rolling()
-	if SCOREP1 + die > 100:
-			print("you cant exceed 100 :)")
-			PERMISSION1 = False
-	if SCOREP2 + die > 100:
-			print("you cant exceed 100 :)")
-			PERMISSION2 = False
-	if TURN%2 == 0 and OLD_CLICKS != NEW_CLICKS and PERMISSION1 == True:			
-		OLD_CLICKS = NEW_CLICKS
-		for _ in range(die):
-			if p1.pos()[0] > 200-X and CHANGED1 is False: 
-				p1.goto(p1.pos()[0], p1.pos()[1] + Y)
-				p1.setheading(180)
-				CHANGED1 = True
-				continue
-			elif p1.pos()[0] < -200 + X  and JUST_STARTED1 is False and CHANGED1 is False: 
-				p1.goto(p1.pos()[0], p1.pos()[1] + Y)
-				p1.setheading(0)
-				CHANGED1 = True
-				continue
-			p1.forward(X)
-			CHANGED1 = False
-		SCOREP1 += die
-		#check for ladder or snake here
-		print("p1",SCOREP1)
-		TURN += 1
-		NEW_CLICKS += 1
-		if SCOREP1 != 1:
-			JUST_STARTED1 = False
-	elif TURN%2 == 1 and OLD_CLICKS != NEW_CLICKS and PERMISSION2 == True:		  	#player 2
-		OLD_CLICKS = NEW_CLICKS
-		for _ in range(die):
-			if p2.pos()[0] > 200-X and CHANGED2 is False: 
-				p2.goto(p2.pos()[0], p2.pos()[1] + Y)
-				p2.setheading(180)
-				CHANGED2 = True
-				continue
-			elif p2.pos()[0] < -200 + X  and JUST_STARTED2 is False and CHANGED2 is False: 
-				p2.goto(p2.pos()[0], p2.pos()[1] + Y)
-				p2.setheading(0)
-				CHANGED2 = True
-				continue
-			p2.forward(X)
-			CHANGED2 = False
-		SCOREP2 += die
-		#check for ladder or snake here
-		print("p2",SCOREP2)
-		TURN += 1
-		NEW_CLICKS += 1
-		if SCOREP2 != 1:
-			JUST_STARTED2 = False
+    global TURN, FORWARD1, FORWARD2, SCOREP1, SCOREP2, OLD_CLICKS, NEW_CLICKS, JUST_STARTED1, CHANGED1, JUST_STARTED2, CHANGED2, PERMISSION1, PERMISSION2
+    die = rolling()
+    if SCOREP1 + die > 100:
+            print("you cant exceed 100 :)")
+            PERMISSION1 = False
+    if SCOREP2 + die > 100:
+            print("you cant exceed 100 :)")
+            PERMISSION2 = False
+    if TURN%2 == 0 and OLD_CLICKS != NEW_CLICKS and PERMISSION1 == True:            
+        OLD_CLICKS = NEW_CLICKS
+        for _ in range(die):
+            if p1.pos()[0] > 200-X and CHANGED1 is False: 
+                p1.goto(p1.pos()[0], p1.pos()[1] + Y)
+                p1.setheading(180)
+                CHANGED1 = True
+                continue
+            elif p1.pos()[0] < -200 + X  and JUST_STARTED1 is False and CHANGED1 is False: 
+                p1.goto(p1.pos()[0], p1.pos()[1] + Y)
+                p1.setheading(0)
+                CHANGED1 = True
+                continue
+            p1.forward(X)
+            CHANGED1 = False
+        SCOREP1 += die
+        #check for ladder or snake here
+        SCOREP1 = check_snake(SCOREP1)
+        SCOREP1 = check_ladder(SCOREP1)
+        position1(SCOREP1)
+        print("p1",SCOREP1)
+        TURN += 1
+        NEW_CLICKS += 1
+        if SCOREP1 != 1:
+            JUST_STARTED1 = False
+    elif TURN%2 == 1 and OLD_CLICKS != NEW_CLICKS and PERMISSION2 == True:          #player 2
+        OLD_CLICKS = NEW_CLICKS
+        for _ in range(die):
+            if p2.pos()[0] > 200-X and CHANGED2 is False: 
+                p2.goto(p2.pos()[0], p2.pos()[1] + Y)
+                p2.setheading(180)
+                CHANGED2 = True
+                continue
+            elif p2.pos()[0] < -200 + X  and JUST_STARTED2 is False and CHANGED2 is False: 
+                p2.goto(p2.pos()[0], p2.pos()[1] + Y)
+                p2.setheading(0)
+                CHANGED2 = True
+                continue
+            p2.forward(X)
+            CHANGED2 = False
+        SCOREP2 += die
+        #check for ladder or snake here
+        SCOREP2 = check_snake(SCOREP2)
+        SCOREP2 = check_ladder(SCOREP2)
+        position2(SCOREP2)
+        print("p2",SCOREP2)
+        TURN += 1
+        NEW_CLICKS += 1
+        if SCOREP2 != 1:
+            JUST_STARTED2 = False
 
 
 
